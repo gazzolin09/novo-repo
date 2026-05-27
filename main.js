b96130857525b5ae307cff3343cbe2df9ce4bb31
gazzolin09 <gabrielle.azzolin.nascimento@escola.pr.gov.br>
2026-05-27 08:31:04 -0300

    ;

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
-for (let i = 0; i < botoes.length; i++) {
-  console.log(i);
-}
-botoes[i].onclick = function () {};
-botoes[i].onclick = function () {
-  botoes[i].classList.add("ativo");
-};
-for (let i = 0; i < botoes.length; i++) {
-  botoes[i].onclick = function () {
-    botoes[i].classList.add("ativo");
-  };
-}
-for (let j = 0; j < botoes.length; j++) {
-  botoes[j].classList.remove("ativo");
-}
-const botoes = document.querySelectorAll(".botao");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
@@ -26,17 +9,3 @@ for (let i = 0; i < botoes.length; i++) {
     botoes[i].classList.add("ativo");
   };
 }
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
-botoes[i].classList.add("ativo");
-textos[i].classList.add("ativo");
-for (let i = 0; i < botoes.length; i++) {
-}
-const contadores = document.querySelectorAll(".contador");
-contadores[0].textContent = "Contagem regressiva";
-const tempoObjetivo1 = new Date("2024-10-05");
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
-
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
